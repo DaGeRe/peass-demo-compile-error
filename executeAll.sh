72c7ca392dc3a95004257875abab6385c63452b3
Matti Wilhelmi <mwilhelmi@gmx.net>
2021-06-08 16:06:17 +0200

    Adapted executeAll.sh to unified demo-project-compile-error

--- executeAll.sh
+++ executeAll.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 DEMO_PROJECT_NAME=demo-project-compile-error
 
 git clone https://github.com/mai13drd/demo-project-compile-error && \
     cd  demo-project-compile-error && \
-    git reset --hard ec6b3b4512d94552a8fa1a12862aae2674869f20 && \
+    git reset --hard 577bb7f03abbb8855232fe22adac9966c7c178fb && \
     cd ..
 
 git clone https://github.com/DaGeRe/peass.git && \
 	cd peass && \
-	./mvnw clean install -DskipTests=true -V
+	./mvnw clean install -DskipTests -V
 
 DEMO_HOME=$(pwd)/../$DEMO_PROJECT_NAME
 DEMO_PROJECT_PEASS=../"$DEMO_PROJECT_NAME"_peass
@@ -17,8 +18,8 @@ DEPENDENCY_FILE=results/deps_"$DEMO_PROJECT_NAME".json
 CHANGES_DEMO_PROJECT=results/changes_"$DEMO_PROJECT_NAME".json
 PROPERTY_FOLDER=results/properties_"$DEMO_PROJECT_NAME"/
 
-RIGHT_SHA="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
-echo "RIGHT_SHA: $RIGHT_SHA"
+VERSION="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
+echo "VERSION: $VERSION"
 
 # It is assumed that $DEMO_HOME is set correctly and PeASS has been built!
 echo ":::::::::::::::::::::SELECT:::::::::::::::::::::::::::::::::::::::::::"
@@ -28,8 +29,8 @@ echo ":::::::::::::::::::::SELECT:::::::::::::::::::::::::::::::::::::::::::"
 #Measure again with a version which has no compile error
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 cd $DEMO_HOME && git pull
-RIGHT_SHA="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
-echo "RIGHT_SHA: $RIGHT_SHA"
+VERSION="$(cd "$DEMO_HOME" && git rev-parse HEAD)"
+echo "VERSION: $VERSION"
 
 cd ../peass
 
@@ -53,49 +54,43 @@ echo "::::::::::::::::::::GETCHANGES::::::::::::::::::::::::::::::::::::::::"
 
 #Check, if $CHANGES_DEMO_PROJECT contains the correct commit-SHA
 TEST_SHA=$(grep -A1 'versionChanges" : {' $CHANGES_DEMO_PROJECT | grep -v '"versionChanges' | grep -Po '"\K.*(?=")')
-if [ "$RIGHT_SHA" != "$TEST_SHA" ]
+if [ "$VERSION" != "$TEST_SHA" ]
 then
-    echo "commit-SHA is not equal to the SHA in $CHANGES_DEMO_PROJECT"
+    echo "commit-SHA ("$VERSION") is not equal to the SHA in $CHANGES_DEMO_PROJECT ("$TEST_SHA")"
     cat results/statistics/"$DEMO_PROJECT_NAME".json
     exit 1
 else
     echo "$CHANGES_DEMO_PROJECT contains the correct commit-SHA."
 fi
 
-# If minor updates to the project occur, the version name may change
-VERSION=$(grep '"testcases" :' -B 1 $EXECUTION_FILE | head -n 1 | tr -d "\": {")
-echo "VERSION: $VERSION"
-
 echo "::::::::::::::::::::SEARCHCAUSE:::::::::::::::::::::::::::::::::::::::"
 ./peass searchcause -vms 5 -iterations 1 -warmup 0 -version $VERSION \
-    -test de.dagere.peass.CalleeTest\#onlyCallMethod1 \
+    -test de.dagere.peass.ExampleTest\#test \
     -folder $DEMO_HOME \
     -executionfile $EXECUTION_FILE
 
 echo "::::::::::::::::::::VISUALIZERCA::::::::::::::::::::::::::::::::::::::"
 ./peass visualizerca -data $DEMO_PROJECT_PEASS -propertyFolder $PROPERTY_FOLDER
 
-#Check, if a slowdown is detected for innerMethod
-STATE=$(grep '"call" : "de.dagere.peass.Callee#innerMethod",\|state' results/$VERSION/de.dagere.peass.CalleeTest_onlyCallMethod1.js \
-    | grep "innerMethod" -A 1 \
+#Check, if a slowdown is detected for Callee#innerMethod
+STATE=$(grep -A21 '"call" : "de.dagere.peass.Callee#innerMethod",' results/$VERSION/de.dagere.peass.ExampleTest_test.js \
     | grep '"state" : "SLOWER",' \
     | grep -o 'SLOWER')
 if [ "$STATE" != "SLOWER" ]
 then
-    echo "State for de.dagere.peass.Callee#innerMethod in de.dagere.peass.CalleeTest#onlyCallMethod1.html has not the expected value SLOWER, but was $STATE!"
-    cat results/$VERSION/de.dagere.peass.CalleeTest_onlyCallMethod1.js
+    echo "State for Callee#innerMethod in de.dagere.peass.ExampleTest_test.js has not the expected value SLOWER, but was $STATE!"
+    cat results/$VERSION/de.dagere.peass.ExampleTest_test.js
     exit 1
 else
-    echo "Slowdown is detected for innerMethod."
+    echo "Slowdown is detected for Callee#innerMethod."
 fi
 
-SOURCE_METHOD_LINE=$(grep "de.dagere.peass.Callee.method1_" results/$VERSION/de.dagere.peass.CalleeTest_onlyCallMethod1.js -A 3 \
+SOURCE_METHOD_LINE=$(grep "Callee.method1_" results/$VERSION/de.dagere.peass.ExampleTest_test.js -A 3 \
     | head -n 3 \
     | grep innerMethod)
 if [[ "$SOURCE_METHOD_LINE" != *"innerMethod();" ]]
 then
     echo "Line could not be detected - source reading probably failed."
-    echo "Line: "
     echo "SOURCE_METHOD_LINE: $SOURCE_METHOD_LINE"
     exit 1
 else
